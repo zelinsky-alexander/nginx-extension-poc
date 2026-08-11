53153b800524079ef5c2cee9d737b20f1aa85b4e
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-08-11 18:15:40 +0300

    Add shared upstream identity state interface

---
+++ src/ngx_http_upstream_identity_state.h
@@ -0,0 +1,14 @@
+#ifndef NGX_HTTP_UPSTREAM_IDENTITY_STATE_H
+#define NGX_HTTP_UPSTREAM_IDENTITY_STATE_H
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+
+ngx_int_t ngx_http_upstream_identity_state_init_zone(ngx_shm_zone_t *shm_zone,
+    void *data);
+
+ngx_int_t ngx_http_upstream_identity_state_track(ngx_shm_zone_t *shm_zone,
+    ngx_log_t *log, const ngx_str_t *upstream, const u_char *peer,
+    size_t peer_len, const char *cert_sha256, const char *spki_sha256);
+
+#endif
