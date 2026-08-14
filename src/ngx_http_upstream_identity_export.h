7c3ca334edbae6c3df3a8d8fd94a4919b76e31cb
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-08-14 18:21:30 +0300

    Use lazy C event exporter configuration

--- src/ngx_http_upstream_identity_export.h
+++ src/ngx_http_upstream_identity_export.h
@@ -6,11 +6,6 @@
 
 #include "ngx_http_upstream_identity_event.h"
 
-ngx_int_t ngx_http_upstream_identity_export_init(ngx_cycle_t *cycle,
-    const ngx_str_t *socket_path);
-
-void ngx_http_upstream_identity_export_close(ngx_cycle_t *cycle);
-
 ngx_int_t ngx_http_upstream_identity_export_event(ngx_log_t *log,
     const ngx_http_upstream_identity_event_t *event,
     const ngx_str_t *upstream, const u_char *peer, size_t peer_len);
