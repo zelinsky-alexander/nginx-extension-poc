ca092cdcc6fa3dc3609df89e5186b96403d064e7
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-08-15 15:55:10 +0300

    Expose shared-state JSON snapshot API

--- src/ngx_http_upstream_identity_state.h
+++ src/ngx_http_upstream_identity_state.h
@@ -11,4 +11,7 @@ ngx_int_t ngx_http_upstream_identity_state_track(ngx_shm_zone_t *shm_zone,
     ngx_log_t *log, const ngx_str_t *upstream, const u_char *peer,
     size_t peer_len, const char *cert_sha256, const char *spki_sha256);
 
+ngx_int_t ngx_http_upstream_identity_state_render_json(
+    ngx_shm_zone_t *shm_zone, ngx_pool_t *pool, ngx_str_t *out);
+
 #endif
