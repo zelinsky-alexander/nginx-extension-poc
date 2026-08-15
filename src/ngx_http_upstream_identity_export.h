7420b2e73077548d0742a96f3f14cb9fed2d82bc
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-08-15 15:57:04 +0300

    Expose first-class history socket configuration

--- src/ngx_http_upstream_identity_export.h
+++ src/ngx_http_upstream_identity_export.h
@@ -6,6 +6,9 @@
 
 #include "ngx_http_upstream_identity_event.h"
 
+ngx_int_t ngx_http_upstream_identity_export_configure(ngx_pool_t *pool,
+    const ngx_str_t *socket_path);
+
 ngx_int_t ngx_http_upstream_identity_export_event(ngx_log_t *log,
     const ngx_http_upstream_identity_event_t *event,
     const ngx_str_t *upstream, const u_char *peer, size_t peer_len);
